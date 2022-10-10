c3c8072210b952da8a0a337e558ccc38733543bd
Rafael <perezaguirrerafael@gmail.com>
2022-10-10 00:38:15 -0500

    part 6 - Estados (useState)

--- webapp/src/index.js
+++ webapp/src/index.js
@@ -1,28 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom/client";
 import Alumno from "./componentes/Usuario";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 const App = () => {
-  let sesion = true;
-  const cerrarSesion = () => {
-    console.log("hola");
-    sesion = false;
-    console.log(sesion);
-  };
+  const [sesion, cambiarSesion] = useState(true);
+  const [contador, setContador] = useState(0);
 
   return (
     <>
       {sesion === true ? (
         <>
+          <h1>{contador}</h1>
           <Alumno />
-          <button onClick={cerrarSesion}>Cerrar sesion</button>
+          <button onClick={() => cambiarSesion(false)}>Cerrar sesion</button>
+          <button onClick={() => setContador(contador+1)}>Sumar</button>
         </>
       ) : (
         <>
           <h2>No has iniciado sesión</h2>
-          <button>Cerrar sesion</button>
+          <button onClick={() => cambiarSesion(true)}>Cerrar sesion</button>
         </>
       )}
     </>
